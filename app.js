b5403644e81ad42d8707b55d73431909d516489d
Pyae Phyo Hlaing <www.pyaephyoehlaing16@gmail.com>
2023-12-11 10:00:03 +0630

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next();
+})
+app.use(express.json());
